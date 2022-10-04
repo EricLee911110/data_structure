b0fb35cca6b0a70e9096cef9927236290c65097a
Eric Lee <79837982+EricLee911110@users.noreply.github.com>
2022-10-04 18:51:16 +0800

    define customerTime & currentTime

--- test.py
+++ test.py
@@ -21,17 +21,25 @@ print("customerList: \n", customerList)
 print("\ncustomerDict: \n", customerDict)
 print("\ncounterDict: \n", counterDict)
 
-idx_customer = 0
-current_time = 0
-while(len(customerDict) != 0):
+idx_in_customer = 0
+idx_out_customer = 0
+currentTime = 0
+
+while (len(customerDict) != 0):
     # if the customer entered the bank
-    if ()
-    customerName = customerList[idx_customer].split(' ')[0]
+    if True:
+        pass 
+
+    customerName = customerList[idx_in_customer].split(' ')[0]
+    idx_in_customer += 1
+    print(customerName)
+    customerTime = customerDict[customerName]
+    
     
     # if finished
     del customerDict[customerName]
-    idx_customer += 1
+    idx_out_customer += 1
 
     # current_time add 1 in each loop
-    current_time += 1
+    currentTime += 1
 
