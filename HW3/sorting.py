11aa971400ee8723ca255edef4f8d7b3f4a1f208
Eric Lee <79837982+EricLee911110@users.noreply.github.com>
2022-11-30 11:51:48 +0800

    insertion Sort

--- HW3/sorting.py
+++ HW3/sorting.py
@@ -14,8 +14,8 @@ def bubbleSort(arr, bubbleSwapCount):
             # tracking how many times we have swaped
             bubbleSwapCount += 1
             if bubbleSwapCount == 5:
-                print(arr[i + 1], arr[i])
-                return arr[i + 1], arr[i], arr
+                #print(arr[i + 1], arr[i])
+                return arr[i], arr[i + 1], arr
             
 
 
@@ -24,7 +24,45 @@ def bubbleSort(arr, bubbleSwapCount):
     else:
         return bubbleSort(arr, bubbleSwapCount)
 
+def insert(arr, small_num_idx):
+    finish = True
 
-bubbleSwap1, bubbleSwap2, bubbleResult = bubbleSort(inputs, 0)
-print(bubbleResult)
+    for i in range(small_num_idx):
+        if arr[i] > arr[i+1]:
+            temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+            finish = False
 
+    if finish == True:
+        return arr
+    else:
+        return insert(arr, small_num_idx)
+
+    
+
+def insertionSort(arr, insertCount):
+    for i in range(len(arr) -1):
+        if arr[i] > arr[i+1]:
+            
+            # insert to the right place
+            arr = insert(arr, i + 1)
+            insertCount += 1
+            
+            if insertCount == 5:
+                #rint("step : ", insertCount)
+                #print(arr)
+                return arr
+        else:
+            insertCount += 1
+            if insertCount < 6:
+                pass
+                #print("step : ", insertCount)
+                #print(arr)
+
+bubbleSwap1, bubbleSwap2, bubbleResult = bubbleSort(inputs[:], 0)
+#print(bubbleSwap1, bubbleSwap2, bubbleResult)
+
+# why 1? Because the first element in the array will be added into sorted_list no matter what, and that counts as a step
+insertionResult = insertionSort(inputs[:], 1)
+print(insertionResult)
